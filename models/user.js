aa29a78001a340e2cf85fa66bc17478583d7d4d0
Robert <robertowusu4199@gmail.com>
2022-03-09 14:45:51 +0000

    all models set

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema ({
+    firstname: {
+        type: String,
+        required: true
+    },
+
+    lastname: {
+        type: String,
+        required: true
+    },
+
+    gender: {
+        type: String, 
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    agents: [ 
+        {
+            id: {type: Schema.Types.ObjectId,ref: "Apartment",}
+        }
+    ]
+
+})
+
+module.exports = mongoose.model("User", UserSchema)
