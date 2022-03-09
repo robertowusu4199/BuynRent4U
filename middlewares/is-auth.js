fc0d8adcddec69926b18615de5fd5ce1578dea5a
Robert <robertowusu4199@gmail.com>
2022-03-09 14:44:44 +0000

    middlewares/auth

---
+++ middlewares/is-auth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken")
+
+const isAuth = (req, res, next) => {
+
+    try {
+        //exctract the authorization from the hearder
+        const authorizationHeader = req.get("Authorization")
+        if (!authorizationHeader)
+            throw new Error("Unauthenticated")
+
+        const token = authorizationHeader.split(" ")[1]
+        console.log(token)
+        //Verify the token with jwt
+        const decodedToken = jwt.verify(token, "superscretkey")
+
+        if(!decodedToken)
+           throw new Error("Unauthorized")
+
+           req.userId = decodedToken.userId;
+        next()
+
+    } catch (error) {
+        console.log(error)
+        res.json({message: error.message})
+    }
+}
+
+module.exports = isAuth
